2fa2e38d80a64d9f050b0ad87bd0ecdaa3106213
Arian Dannemann <arian.dannemann@gmail.com>
2019-05-18 17:31:08 +0200

    Switched to cooldown system

--- Wands/src/com/Wands/Wand.java
+++ Wands/src/com/Wands/Wand.java
@@ -1,26 +1,33 @@
 package com.Wands;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public abstract class Wand implements Listener {
 	
 	protected Main main;
 	protected String name;
-	protected int cost;
+	protected int cooldown;
 	
-	public Wand(Main main, String name, int cost) {
+	protected List<String> playersOnCooldown = new ArrayList<>();
+	
+	public Wand(Main main, String name, int cooldown) {
 		// Register events as listener
 		main.getServer().getPluginManager().registerEvents(this, main);
 		
 		// Set local variables to the given variables
 		this.main = main;
 		this.name = name;
-		this.cost = cost;
+		this.cooldown = cooldown;
 	}
 	
 	@EventHandler
@@ -39,14 +46,34 @@ public abstract class Wand implements Listener {
 					&& item.getItemMeta() != null
 					&& item.getItemMeta().getDisplayName() != null) {
 				
-				// Check if player is holding a fire ball wand
+				// Check if player is holding the correct wand
 				if (item.getItemMeta().getDisplayName().equals(name)) {
-					if (InventoryManager.removeGunpowderFromPlayerInventory(player, cost)) {
-						runAction(player);
+					
+					// Don't execute any action if the player is on cooldown
+					if (playersOnCooldown.contains(player.getName())) {
+						
+						// Play a sound to let the player know theres still a cooldown
+						player.getWorld().playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1, 1);
+						
+						// Return the function
+						return;
 					}
-					else {
-						player.sendMessage("You do not have enough gunpowder to use this wand");
-					}
+					
+					// Run the wand action
+					runAction(player);
+
+					// Add player to cooldown list
+					playersOnCooldown.add(player.getName());
+					
+					// This runnable will remove the player from cooldown list after a given time
+					BukkitRunnable runnable = new BukkitRunnable() {	
+						@Override
+						public void run() {
+							// Remove player from cooldown list
+							playersOnCooldown.remove(player.getName());
+						}
+					};
+					runnable.runTaskLater(main, 20 * cooldown);
 				}
 			}
 		}
