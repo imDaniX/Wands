978b4f20d72f6a5c266c5f18ca726227f1dd4149
Arian Dannemann <arian.dannemann@gmail.com>
2019-05-18 18:23:07 +0200

    Fixed /wands give command to work with new spawning system

--- Wands/src/com/Wands/CommandHandler.java
+++ Wands/src/com/Wands/CommandHandler.java
@@ -1,11 +1,9 @@
 package com.Wands;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import com.Wands.InventoryManager;
+import org.bukkit.inventory.ItemStack;
 
 public class CommandHandler {
 	
@@ -22,64 +20,30 @@ public class CommandHandler {
 			
 			// Check if the player has operator rights
 			if (!player.isOp()) {
-				player.sendMessage("Only operators can recieve wands over commands");
+				player.sendMessage("Only operators can use wand commands");
 				return;
 			}
 			
-			if (args.length == 2 && args[0].equals("give")) {				
-				if (args[1].equals("fireball")) {
-					InventoryManager.giveWandToPlayer(player, ChatColor.RED + "Fireball Wand");
-				}
+			if (args.length == 2 && args[0].equals("give")) {		
 				
-				if (args[1].equals("ice")) {
-					InventoryManager.giveWandToPlayer(player, ChatColor.BLUE + "Ice Wand");
-				}
-				
-				if (args[1].equals("earth")) {
-					InventoryManager.giveWandToPlayer(player, ChatColor.GRAY + "Earth Wand");
-				}
-				
-				if (args[1].equals("teleport")) {
-					InventoryManager.giveWandToPlayer(player, ChatColor.LIGHT_PURPLE + "Teleport Wand");
-				}
-				
-				if (args[1].equals("summoners")) {
-					InventoryManager.giveWandToPlayer(player, ChatColor.AQUA + "Summoners Wand");
-				}
-				
-				if (args[1].equals("lightning")) {
-					InventoryManager.giveWandToPlayer(player, ChatColor.DARK_BLUE + "Lightning Wand");
-				}
-				
-				if (args[1].equals("rocket")) {
-					InventoryManager.giveWandToPlayer(player, ChatColor.YELLOW + "Rocket Wand");
-				}
-				
-				if (args[1].equals("craftsman")) {
-					InventoryManager.giveWandToPlayer(player, ChatColor.DARK_AQUA + "Craftsman Wand");
-				}
-				
-				if (args[1].equals("cloud")) {
-					InventoryManager.giveWandToPlayer(player, ChatColor.RESET + "Cloud Wand");
-				}
-				
-				if (args[1].equals("trickery")) {
-					InventoryManager.giveWandToPlayer(player, ChatColor.DARK_GRAY + "Trickery Wand");
-				}
-				
-				if (args[1].equals("pumpkin")) {
-					InventoryManager.giveWandToPlayer(player, ChatColor.GOLD + "Pumpkin Wand");
-				}
-			}
-			
-			if (args.length == 1 && args[0].equals("free")) {
-				Main.costEnabled = !Main.costEnabled;
-				
-				if (Main.costEnabled) {
-					player.sendMessage("Wands will now cost gunpowder again");
-				}
-				else {
-					player.sendMessage("All wands are now free to use");
+				// Loop through all possible 
+				for (Wand wand : Main.wandVariations) {
+					
+					// Check if wand is the one requested
+					if (wand.name.toLowerCase().contains(args[1].toLowerCase())) {
+						
+						// Create wand item
+						ItemStack wandItem = wand.createWandItem();
+						
+						// Add wand to players inventory
+						player.getInventory().addItem(wandItem);
+						
+						// Notify the player
+						player.sendMessage("You have recieved a " + wand.name);
+						
+						// Return the function
+						return;
+					}
 				}
 			}
 		}
