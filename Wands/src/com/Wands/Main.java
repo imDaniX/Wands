db6390127dd059ec0ec271c9249435692ba1e945
Arian Dannemann <arian.dannemann@gmail.com>
2019-05-18 17:41:16 +0200

    Adjusted cooldowns of wands once again

--- Wands/src/com/Wands/Main.java
+++ Wands/src/com/Wands/Main.java
@@ -20,17 +20,21 @@ public class Main extends JavaPlugin {
 	}
 	
 	void initiateWands() {
-		new FireballWand(this, ChatColor.RED + "Fireball Wand", 2);
-		new IceWand(this, ChatColor.BLUE + "Ice Wand", 10);
-		new EarthWand(this, ChatColor.GRAY + "Earth Wand", 30);
-		new TeleportWand(this, ChatColor.LIGHT_PURPLE + "Teleport Wand", 5);
-		new SummonersWand(this, ChatColor.AQUA + "Summoners Wand", 30);
-		new LightningWand(this, ChatColor.DARK_BLUE + "Lightning Wand", 60);
-		new RocketWand(this, ChatColor.YELLOW + "Rocket Wand", 3);
-		new CraftingWand(this, ChatColor.DARK_AQUA + "Craftsman Wand", 0);
-		new WoolWand(this, ChatColor.RESET + "Cloud Wand", 30);
-		new TrickeryWand(this, ChatColor.DARK_GRAY + "Trickery Wand", 15);
-		new PumpkinWand(this, ChatColor.GOLD + "Pumpkin Wand", 30);
+		new CraftingWand(this, 			ChatColor.DARK_AQUA + "Craftsman Wand", 	0);
+		
+		new FireballWand(this, 			ChatColor.RED + "Fireball Wand", 			5);
+		new TeleportWand(this, 			ChatColor.LIGHT_PURPLE + "Teleport Wand", 	5);
+		new RocketWand(this,			ChatColor.YELLOW + "Rocket Wand", 			5);
+		
+		new TrickeryWand(this, 			ChatColor.DARK_GRAY + "Trickery Wand", 		20);
+		new IceWand(this, 				ChatColor.BLUE + "Ice Wand", 				20);
+		
+		new WoolWand(this, 				ChatColor.RESET + "Cloud Wand", 			30);
+		new PumpkinWand(this, 			ChatColor.GOLD + "Pumpkin Wand",			30);
+		new EarthWand(this, 			ChatColor.GRAY + "Earth Wand", 				30);
+		
+		new SummonersWand(this, 		ChatColor.AQUA + "Summoners Wand", 			60);
+		new LightningWand(this, 		ChatColor.DARK_BLUE + "Lightning Wand", 	60);
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
